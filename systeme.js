e26567feeb9c6319f8e644a2957f14d423996b76
SaionJiKayle <62202896+SaionJiKayle@users.noreply.github.com>
2020-03-17 09:12:03 +0100

    Create systeme.js

---
+++ systeme.js
@@ -0,0 +1,4 @@
+	function myFunction() {
+							var test = document.form1.input.value;
+  							document.getElementById("myFrame").src = "https://api.download-lagu-mp3.com/@api/button/mp3/"+test;
+	}document.getElementById('do').onclick = myFunction;
